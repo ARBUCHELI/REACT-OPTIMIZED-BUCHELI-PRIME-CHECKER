2845288a292ca826aea6e98c133ff135ef89e200
ARBUCHELI <radio_seattle@hotmail.com>
2023-03-26 12:45:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { IsPrime } from './IsPrime';
 
-function App() {
+import './styles.css';
+
+export const App = () => {
+  const [number, setNumber] = useState(8191);
+  const [showExplainer, setShowExplainer] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{textAlign: "center", paddingTop: "1em", fontFamily: "monospace", fontWeight: "bold", fontSize: "3em", color: "#bd5734 "}}>BUCHELI PRIME CHECKER</h1>
+      <div className='container' style={{width: "50%", margin: "auto", paddingTop: "2%"}}>
+        <div className='e2-container'>
+          <div className='e2-header'>
+            <div>
+              <h2>Prime checker</h2>
+              <p>See if a number is prime by typing below</p>
+            </div>
+            <button
+              className='e2-button'
+              onClick={() => setShowExplainer(!showExplainer)}
+            >
+              {showExplainer ? 'Hide' : 'Show'} Explainers
+            </button>
+          </div>
+          <input
+            className='e2-input'
+            onChange={(e) => setNumber(e.target.value)}
+            value={number}
+            type='number'
+          />
+          <IsPrime number={number} showExplainer={showExplainer} />
+        </div>
+      </div>
+      <footer style={{textAlign: "center", paddingTop: "2em", fontFamily: "monospace", fontWeight: "bold", fontSize: "2em", color: "#bd5734 "}}>Bucheli Web © 2023</footer>
+    </>
   );
-}
+};
 
 export default App;
